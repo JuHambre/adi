c1c7de4413b869cd20e520be0165364d44205d3c
adi <adi@LUbuntu-ADI.(none)>
2013-11-01 20:52:16 +0100

    Registro completado

--- funciones.js
+++ funciones.js
@@ -94,7 +94,7 @@ function peticionAJAX(){
                 localStorage.login = login;
                 datosUsuario(login);
             })
-            .fail(function(jqXHR, textStatus, errorThrown){
+            .fail(function(jqXHR, textStatus, errorThrown) {
                  jqXHR.statusCode(
                      {
                          400: function() {
@@ -106,6 +106,83 @@ function peticionAJAX(){
                      }
                  );
             });
+    }
+    else{
+        alerta("Alguno de los campos esta vacio", "info");
+    }
+}
+
+function notNull2(){
+    if($('#loginRegistro').val()!='' && $('#contraseñaRegistro').val()!='' && $('#rContraseñaRegistro').val()!=''&& $('#nombreRegistro').val()!='' && $('#apellidosRegistro').val()!=''){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function contraseñaIgual(){
+    if($('#contraseñaRegistro').val()==$('#rContraseñaRegistro').val()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function comprobarRegistro(){
+
+    var login= $('#loginRegistro').val();
+
+    $.ajax({
+        url: 'api/loginDisponible/'+login,
+        method: 'GET'
+    })
+        .done(function(data, textStatus, jqXHR) {
+            if(data=='OK'){
+                $('#loginCogido').hide();
+                $('#loginCorrecto').show();
+            }
+            else{
+                $('#loginCorrecto').hide();
+                $('#loginCogido').show();
+            }
+        })
+}
+
+function peticionAJAX2(){
+
+    if(notNull2()){
+        if(contraseñaIgual()){
+            var json={
+                login:          $('#loginRegistro').val(),
+                password:       $('#contraseñaRegistro').val(),
+                nombre:         $('#nombreRegistro').val(),
+                apellidos:      $('#apellidosRegistro').val()
+            }
+            var jsonstgf = JSON.stringify(json);
+            $.ajax({
+                url:'api/usuarios',
+                method: 'POST',
+                contentType: 'application/json',
+                data:jsonstgf
+            })
+                .done(function(data, textStatus, jqXHR) {
+                    alerta("Registrado en la Batcueva correctamente", "success");
+                })
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    jqXHR.statusCode(
+                        {
+                            400: function() {
+                                alerta("Los datos son incorrectos", "danger");
+                            }
+                        }
+                    )
+                })
+        }
+        else{
+            alerta("Las contraseñas no coinciden", "info");
+        }
     }
     else{
         alerta("Alguno de los campos esta vacio", "info");
